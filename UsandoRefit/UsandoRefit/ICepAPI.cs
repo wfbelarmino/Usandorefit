dd1e79c772191ba96ebd33a39a960bcf8f056947
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2025-09-16 14:51:43 -0300

    Add files via upload

---
+++ UsandoRefit/UsandoRefit/ICepAPI.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+
+
+namespace UsandoRefit
+{
+    public interface ICepAPI
+    {
+        [Get("/ws/{cep}/json/")]
+       
+        Task<CepResponse> ConsultaCep(string cep);
+
+    }
+}
---
+++ UsandoRefit/UsandoRefit/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Refit;
+using System.ComponentModel.Design;
+using UsandoRefit;
+
+Console.WriteLine("Digite o CEP.....");
+var cep = Console.ReadLine();
+
+var service = RestService.For<ICepAPI>("https://viacep.com.br");
+var retorno = await service.ConsultaCep(cep);
+
+if(retorno != null)
+{
+    Console.WriteLine($"Logradouro: {retorno.Logradouro}");
+    Console.WriteLine($"Bairro: {retorno.Bairro}");
+    Console.WriteLine($"Cidade: {retorno.Localidade}");
+    Console.WriteLine($"Estado: {retorno.UF}");
+}
